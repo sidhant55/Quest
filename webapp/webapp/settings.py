3a4241bbc06c96fce50a3093ddd453b71db78c18
Sidhant Mishra <sidhant.plb@gmail.com>
2017-11-09 10:24:36 +0530

    password

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['localhost']
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sidhant97iem@gmail.com'
-EMAIL_HOST_PASSWORD = 'jho5t5555'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 
 
